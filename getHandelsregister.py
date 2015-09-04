c77dbcd2503e2dbfe3c6de9c2e74259476e778d3
Fei Guo <guofei89@gmail.com>
2015-09-04 23:33:21 +0200

    loop through the whole handelsregister -- 1.5h

--- getHandelsregister.py
+++ getHandelsregister.py
@@ -1,7 +1,5 @@
 #-*- coding: UTF-8 -*-
-
 import sys
-import time
 import urllib
 import urllib2
 import requests
@@ -18,32 +16,45 @@ headers=[{'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1
          {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'}];
 
 def handelsregister_spider():
+    for pageNum in range(0, 10500, 1500):
+        getRegisterHtml(pageNum)
+def getRegisterHtml(pageNum):
     register_list = [];
 
     firma = "stiftung"
-    #url="http://zefix.admin.ch/WebServices/Zefix/Zefix.asmx/SearchFirm?name=" + firma + "&suche_nach=aktuell";
-    url="http://zh.powernet.ch/webservices/inet/HRG/HRG.asmx/getHRGHTML?chnr=0207901300&amt=020&toBeModified=0&validOnly=1&lang=1&sort=0"
-    print "Pulling information from [%s]" % url;
+    url="http://zefix.admin.ch/WebServices/Zefix/Zefix.asmx/SearchFirm?name=" + firma + "&suche_nach=aktuell&posMin=" + str(pageNum);
+    soup = BeautifulSoup(read(url));
+    all_registers_link = soup.findAll('font')[0].findAll('a', href=True)
+    if all_registers_link == None:
+        print "There is handelsregister yet.";
+        return;
+    count = 0;
+    for register_link in all_registers_link:
+        if "HTML" in register_link['href']:
+            count += 1
+            getXmlLink(register_link['href'])
+    print count
+    return register_list
 
-    #Last Version
+def getXmlLink(url):
+    soup = BeautifulSoup(read(url));
+    excerpt = soup.findAll(lambda tag: (tag.name == 'a' and tag.text == '<Excerpt>'), href=True)
+    if (excerpt):
+        xml_link = excerpt[0]['href']
+    else:
+        print "empty found"
+        return;
+    print xml_link
+
+def read(url):
+    print "Pulling information from [%s]" % url;
     try:
         req = urllib2.Request(url, headers=headers[1]);
         source_code = urllib2.urlopen(req).read();
         plain_text=str(source_code);
+        return plain_text;
     except (urllib2.HTTPError, urllib2.URLError), e:
         print e
-    #plain_text = '<p><b>"Gottlieb Fischer-Stiftung" im Bergli Stans</b><i> in <a target="_top" href="https://nw.chregister.ch">Stans</a></i>, Stiftung, <a target="result" href="/WebServices/Zefix/Zefix.asmx/ShowFirm?parId=272375&amp;parChnr=CH-150.7.000.034-0&amp;language=1">+</a>, <a target="_blank" href="https://nw.chregister.ch/cr-portal/auszug/zefix.xhtml?uid=CHE-114.513.996&amp;lang=de">CHE-114.513.996</a>, <a target="_blank" href="https://nw.chregister.ch/cr-portal/auszug/zefix?uid=CHE-114.513.996&amp;lang=de">PDF</a></p>'
-    soup = BeautifulSoup(plain_text);
-    #all_registers_link = soup.findAll('font')[0].findAll('a')
-    all_registers_link = soup.findAll(lambda tag: (tag.name == 'a' and tag.text == '<Excerpt>'), href=True)
-    if all_registers_link == None:
-        print "There is handelsregister yet.";
-        return;
-    for register_link in all_registers_link:
-        print register_link['href']
-    
-    return register_list
-
 
 
 if __name__=='__main__':
